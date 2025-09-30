4dca2b628f312ccb8ed9688713006c850eaccf6a
keeterly <30454504+keeterly@users.noreply.github.com>
2025-09-30 05:28:07 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
-// Install: take control without waiting
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
-
-// Activate: claim control of all clients immediately
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
