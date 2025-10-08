f2521da75bc9fc89bf45580d4e9c85d6d5efda41
keeterly <30454504+keeterly@users.noreply.github.com>
2025-10-08 07:51:29 +0700

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,8 +1,8 @@
 // bridge.js (at project root)
 import * as Engine from "./src/engine/index.js";
-import * as UI from "./src/ui/index.js";
-import "./src/ui/drag.js";
 import * as UI from './ui.js';
+import "./src/ui/drag.js";
+
 
 export function exposeToWindow(){
   const g = (window.game && typeof window.game.dispatch==='function')
