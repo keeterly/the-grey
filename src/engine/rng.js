b809601b9d9e657353822c89a50ecfe0d9e281ed
keeterly <30454504+keeterly@users.noreply.github.com>
2025-10-09 09:36:19 +0700

    Update rng.js

--- src/engine/rng.js
+++ src/engine/rng.js
@@ -1,1 +1,1 @@
-
+export function shuffle(a){ for(let i=a.length-1;i>0;i--){const j=(Math.random()*(i+1))|0; [a[i],a[j]]=[a[j],a[i]]} return a; } export function uid(){ return Math.random().toString(36).slice(2,9); }
