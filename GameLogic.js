05bdacdfa526bcd6cbca5fb1ed0548ef29063faf
keeterly <30454504+keeterly@users.noreply.github.com>
2025-10-12 04:50:59 +0700

    Update GameLogic.js

--- GameLogic.js
+++ GameLogic.js
@@ -1,1 +1,6 @@
-export const PLACEHOLDER=true;
+// ensure these are declared in the file:
+export function initState(opts = {}) { /* ... */ }
+export function serializePublic(state) { /* ... */ }
+
+// if you already have the functions but not exported, append:
+export { initState, serializePublic };
